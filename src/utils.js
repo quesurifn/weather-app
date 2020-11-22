eebf361a2f43c6cdb5128ce285c06525cbf45b36
Kyle Fahey <kyle.c.r.fahey@gmail.com>
2020-11-22 18:31:48 -0500

    Add in api key and url

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,16 @@
-const fetchWeather = cityName => {
-  const API_KEY = "<INSERT_API_KEY>";
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const fetchWeather = async (zipCode) => {
+  const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
   // TODO: fetch weather forecast from endpoint
   // from https://openweathermap.org/api
+
+  const url = `api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${API_KEY}`
+  await axios.get(url)
 };
+
+export {
+  fetchWeather
+}
