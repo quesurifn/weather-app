22e13b150ad3f8c3a464326fb4636b2f8bac4d9f
Kyle Fahey <kyle.c.r.fahey@gmail.com>
2020-11-22 18:58:58 -0500

    Add in call to openweather and css for weather card

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import qs from 'querystring';
+
+import { fetchWeather } from '../utils';
 import { ReactComponent as Logo } from "../images/sun.svg";
 import "../App.css";
 
+
 function App() {
-  return <h1>Weather</h1>;
+  const [count, setCount] = useState(0);
+  const [locationData, setLocationData] = useState({});
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+    const parsedQuery = qs.parse(window.location.search.replace('?', ''));
+    const { location } = parsedQuery
+    if(location && /\d{5}/.test(location)) {
+      try {
+        const fetchData = async () => {
+          const weather = await fetchWeather(location)
+          console.log(weather.data)
+          setLocationData(weather.data)
+        };
+        fetchData();
+      } catch(e) {
+        setError(`There was an error retrieving weather data for zip code: ${location}`)
+      }
+    }
+  }, []);
+
+  return (
+      <main>
+        <h1>Weather</h1>
+
+
+
+      </main>
+
+      );
 }
 
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config()
 
-const fetchWeather = async (zipCode) => {
+const fetchWeather = zipCode => {
   const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
   // TODO: fetch weather forecast from endpoint
   // from https://openweathermap.org/api
 
-  const url = `api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${API_KEY}`
-  await axios.get(url)
+  const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${API_KEY}`
+  return axios.get(url)
 };
 
 export {
