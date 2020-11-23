5962d550c3ac352c46b5820c1cb54a199d8991f2
Kyle Fahey <kyle.c.r.fahey@gmail.com>
2020-11-22 20:32:50 -0500

    Update

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,20 @@
 import React, {useState, useEffect} from "react";
 import qs from 'querystring';
 
-import { fetchWeather } from '../utils';
-import { ReactComponent as Logo } from "../images/sun.svg";
+
+import { fetchWeather, parseWeatherData } from '../utils';
+import { Button, Input } from 'semantic-ui-react';
+import WeatherCard from './weatherCard'
 import "../App.css";
+
+import 'semantic-ui-css/semantic.min.css';
 
 
 function App() {
   const [count, setCount] = useState(0);
-  const [locationData, setLocationData] = useState({});
+  const [locationData, setLocationData] = useState(null);
   const [error, setError] = useState("")
+  const [unit, setUnit] = useState('imperial')
 
   useEffect(() => {
     const parsedQuery = qs.parse(window.location.search.replace('?', ''));
@@ -17,9 +22,10 @@ function App() {
     if(location && /\d{5}/.test(location)) {
       try {
         const fetchData = async () => {
-          const weather = await fetchWeather(location)
-          console.log(weather.data)
-          setLocationData(weather.data)
+          const weather = await fetchWeather(location, unit)
+          const parsedWeather = parseWeatherData(weather.data)
+          console.log(parseWeatherData)
+          setLocationData(parsedWeather)
         };
         fetchData();
       } catch(e) {
@@ -32,11 +38,29 @@ function App() {
       <main>
         <h1>Weather</h1>
 
+        {!locationData &&
+          <form>
+            <p>
+              Enter Your Zip Code
+            </p>
+            <Input type="number" name="location" placeholder="Your zip code" min="00000" max="99999"/>
+            <Button type="submit">Find Weather</Button>
+          </form>
+        }
 
+        {locationData && 
+          <div>
+            {locationData.map((e, idx) => {
+              return <WeatherCard key={idx} day={e}/>
+            })}
+          </div>
+        }
 
+        {error &&
+          <p>There was an error, please try again later.</p>
+        }
       </main>
-
-      );
+    );
 }
 
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,46 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
+import _ from 'lodash';
 dotenv.config()
 
-const fetchWeather = zipCode => {
+const fetchWeather = (zipCode, unit) => {
   const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
   // TODO: fetch weather forecast from endpoint
   // from https://openweathermap.org/api
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${API_KEY}`
+  const url = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&units=${unit}&appid=${API_KEY}`
   return axios.get(url)
 };
 
+const findCondition = (condition) => {
+  if(~condition.indexOf('Rain')) {
+    return 'rainy';
+  } else if(~condition.indexOf('Clouds')) {
+    return 'cloudy';
+  } else if (~condition.indexOf('Sun')){
+    return 'sunny';
+  } else {
+    return 'cloudy'
+  }
+}
+
+const parseWeatherData = (data) => {
+  data.list = _.chunk(data.list, 8)
+  const parsedDays = []
+  const days = data.list.map((day) => {
+    const dailyTemps = day.map(e => e.main.temp)
+    const maxTemp = Math.max(dailyTemps)
+    const lowTemp = Math.min(dailyTemps)
+    const conditionImage = findCondition(day[3].weather.main)
+    const dayOfWeek = new Date(day.dt).toLocaleDateString('en-US', { weekday: 'long' })
+    days.push({maxTemp, lowTemp, conditionImage, dayOfWeek})
+  });
+  return parsedDays;
+}
+
+
+
 export {
-  fetchWeather
+  fetchWeather,
+  parseWeatherData
 }
